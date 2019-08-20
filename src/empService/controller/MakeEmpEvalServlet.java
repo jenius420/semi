271c9aa43a51e8e7f47d55e01a9409426a4a366e
earl420 <jenius420@daum.net>
2019-08-21 08:37:44 +0900

    fg

--- src/empService/controller/MakeEmpEvalServlet.java
+++ src/empService/controller/MakeEmpEvalServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MakeEmpEvalServlet
  */
-@WebServlet("/")
+@WebServlet("/emp.es")
 public class MakeEmpEvalServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
