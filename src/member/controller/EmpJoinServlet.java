617faa6bfdeb860060247df9d77f07d78f226536
kj1kj2kj3 <kjhstan@gmail.com>
2019-08-29 01:14:33 +0900

    123123

--- src/member/controller/EmpJoinServlet.java
+++ src/member/controller/EmpJoinServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class OwnerJoinMember
+ * Servlet implementation class EmpJoinMemberServlet
  */
-@WebServlet("/ownerJoinMember.me")
-public class OwnerJoinMemberServlet extends HttpServlet {
+@WebServlet("/empJoin.me")
+public class EmpJoinServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public OwnerJoinMemberServlet() {
+    public EmpJoinServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +27,7 @@ public class OwnerJoinMemberServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.getRequestDispatcher("views/member/ownerJoinMember.jsp").forward(request, response);
+		request.getRequestDispatcher("views/member/empJoin.jsp").forward(request, response);
 	}
 
 	/**
