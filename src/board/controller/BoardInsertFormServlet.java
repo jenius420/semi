081a42518b1dcb2f3a1a5ed26372980cc2803350
tntnfdj <tntnfdj@naver.com>
2019-08-29 13:39:20 +0900

    ㅇㅇㅇ

--- src/board/controller/BoardInsertFormServlet.java
+++ src/board/controller/BoardInsertFormServlet.java
@@ -25,8 +25,9 @@ public class BoardInsertFormServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		request.getRequestDispatcher("views/board/boardInsertForm.jsp").forward(request, response);
+
 	}
 
 	/**
